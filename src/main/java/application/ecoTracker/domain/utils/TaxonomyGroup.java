506d96204938562a90f9823e76ceb5167387a15a
mfouillen <mathis.fouillen@etudiant.univ-rennes.fr>
2023-10-03 17:45:29 +0200

    Created domain.utils

---
+++ src/main/java/application/ecoTracker/domain/utils/TaxonomyGroup.java
@@ -0,0 +1,19 @@
+package application.ecoTracker.domain.utils;
+
+public enum TaxonomyGroup {
+    
+    Animals,
+    Fish,
+    Bird,
+    Reptile,
+    Amphibian,
+    Mammal,
+    Arachnid,
+    Insect,
+    Plant,
+    Fungi,
+    Protozoan,
+    Mollusk,
+    Kelp
+    
+}
