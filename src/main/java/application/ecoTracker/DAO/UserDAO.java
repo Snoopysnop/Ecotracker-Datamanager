cba0b78c66a38eb514aaaeb54a50a0b058ecf36d
mfouillen <mathis.fouillen@etudiant.univ-rennes.fr>
2023-10-04 12:06:57 +0200

    Created UserDAO

---
+++ src/main/java/application/ecoTracker/DAO/UserDAO.java
@@ -0,0 +1,12 @@
+package application.ecoTracker.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import application.ecoTracker.domain.User;
+import jakarta.transaction.Transactional;
+
+
+@Transactional
+public interface UserDAO extends JpaRepository<User, Long> {
+    
+}
