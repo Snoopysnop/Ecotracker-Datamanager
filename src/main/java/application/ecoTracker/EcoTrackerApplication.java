fd3d95366c630ad660129464208c20d6bc9ee7d0
mfouillen <mathis.fouillen@univ-rennes1.fr>
2023-12-05 23:31:43 +0100

    removed unused import

--- src/main/java/application/ecoTracker/EcoTrackerApplication.java
+++ src/main/java/application/ecoTracker/EcoTrackerApplication.java
@@ -3,7 +3,6 @@ package application.ecoTracker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.Statement;
