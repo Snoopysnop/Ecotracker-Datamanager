b9e8415bf5c35c6b67957bcdd38ef729fa929d99
mfouillen <mathis.fouillen@univ-rennes1.fr>
2023-12-06 14:32:39 +0100

    Revert "Fixed image data length"
    This reverts commit 19b692750520393edfb24101c777e68a912f13e6.

--- src/main/java/application/ecoTracker/EcoTrackerApplication.java
+++ src/main/java/application/ecoTracker/EcoTrackerApplication.java
@@ -1,32 +1,13 @@
 package application.ecoTracker;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.Statement;
 
 @SpringBootApplication
-public class EcoTrackerApplication implements CommandLineRunner{
-
-	private final DataSource dataSource;
-
-    public EcoTrackerApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+public class EcoTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcoTrackerApplication.class, args);
 	}
 
-	@Override
-    public void run(String... args) throws Exception {
-        try (Connection connection = dataSource.getConnection()) {
-            Statement statement = connection.createStatement();
-
-            statement.executeUpdate("ALTER TABLE ecoTracker.Campaign MODIFY COLUMN image LONGBLOB;");
-			statement.executeUpdate("ALTER TABLE ecoTracker.Observation MODIFY COLUMN imageList LONGBLOB;");
-        }
-	}
 }
